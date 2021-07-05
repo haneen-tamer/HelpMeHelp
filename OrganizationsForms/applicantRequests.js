5518222605bb8682903d6074c18dd4bd5f5612e0
yomnayasser <yomnayasser31@gmail.com>
2021-07-05 20:15:59 +0200

    All Organization Pages integereted with backend

--- OrganizationsForms/applicantRequests.js
+++ OrganizationsForms/applicantRequests.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity , ScrollView} from 'react-native';
 import ApplicantCard from './applicantCard';
 
@@ -7,30 +7,49 @@ import ApplicantCard from './applicantCard';
 
 export default function App({navigation}) {
 
-  const [user, setUser] = useState([
-    {name:'Yomna Yasser',age:'22',userStatus:'Pending',id:'1',email:'yomna@gmail.com', governorate:'Cairo',contribution:'1',username:'YY'},
-    {name:'Haneen',age:'21',userStatus:'Pending',id:'2',email:'sssss', governorate:'ddd',username:'HT'},
-    {name:'Assem',age:'23',userStatus:'Pending',id:'3',email:'sssss', governorate:'ddd',username:'AM'},
-    {name:'Sara',age:'21',userStatus:'Pending',id:'4',email:'sssss', governorate:'ddd',username:'SF'},
-    {name:'Raghda',age:'22',userStatus:'Pending',id:'5',email:'sssss', governorate:'ddd',username:'RM'},
-    {name:'Hager',age:'21',userStatus:'Pending',id:'6',email:'sssss', governorate:'ddd',username:'HM'},
-  ])
-
+  const [ID,setID]=useState(navigation.getParam('ID'))
+  const [user, setUser] = useState(null)
+  const [found,setFound]=useState(false);
+  const [noApplicants,setNoApplicants]=useState(false);
+  useEffect(() => {
+    setFound(true);
+      fetch("http://10.0.2.2:8080/orgApplicants/"+ID, {
+        method: 'GET',
+    })
+    .then(res=>res.json())
+    .then(json => {
+      
+        setUser(json)
+        console.log(user)
+      
+      
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+  }, []);
+  console.log(user)
+  // if(user==null)
+  // {
+  //   setNoApplicants(true);
+  // }
   return (
     <ScrollView>
       <View style={styles.container}>
         <View style={styles.cardStyle}>
+          {found &&
           <FlatList
           keyExtractor={(item) => item.id} 
           data={user} 
           renderItem={({ item }) => ( 
             
-              <ApplicantCard item={item} navigation={navigation}/>
+              <ApplicantCard item={item} navigation={navigation} ID={ID}/>
               
           )}
           
           />
-         
+          }
+
 
         </View>
 

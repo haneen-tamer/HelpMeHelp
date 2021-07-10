dbfeb8b4d6a2386b3d1e59cd2a8972fc8efb022d
sarafares <76113233+sarafares@users.noreply.github.com>
2021-07-10 09:41:50 +0200

    search update

--- forms/AllChats.js
+++ forms/AllChats.js
@@ -6,36 +6,73 @@ import Header from '../shared/header';
 export default function chat({navigation}) {
     let [chatInput, setChatInput] = useState("");
     const [username,setUsername]=useState( navigation.dangerouslyGetParent().getParam('User_Username'));
+    const [org_username,setOrgUsername]=useState( navigation.dangerouslyGetParent().getParam('OrgUsername'));
     let [chatItemList, setChatItemList] = useState([]);//useState<chatItem[]>([]);
     let[refresh,setRefresh]=useState(false);
+    let[refresh1,setRefresh1]=useState(false);
     //let username = navigation.getParam('username');
     const get_allchats=()=>{
-      console.log(username);
-      fetch("http://10.0.2.2:8080/allchats/"+username,{
-          method:"get"
-      })
-        .then(res=>res.json())
-        .then(json =>{
-          for(let i=0;i<json.length;i++)
-          {
-            chatItemList.push(
-            {
-              id: Math.random().toString(36).substring(7),
-              with: json[i].Username1,
-              text: "haha"
-            });
-          }
-          //console.log(chatItemList);
-          setRefresh(true);
+      
+      if(typeof username != 'undefined'){
+        //setRefresh1(true);
+        console.log(username);
+        return fetch("http://10.0.2.2:8080/allchats/"+username,{
+            method:"get"
         })
+          .then(res=>res.json())
+          .then(json =>{
+            for(let i=0;i<json.length;i++)
+            {
+              chatItemList.push(
+              {
+                id: Math.random().toString(36).substring(7),
+                with: json[i].Username1,
+                text: "haha"
+              });
+            }
+            //console.log(chatItemList);
+            setRefresh(true);
+          })
+      }
+      else{
+        console.log(org_username);
+        return fetch("http://10.0.2.2:8080/allchats/"+org_username,{
+            method:"get"
+        })
+          .then(res=>res.json())
+          .then(json =>{
+            for(let i=0;i<json.length;i++)
+            {
+              chatItemList.push(
+              {
+                id: Math.random().toString(36).substring(7),
+                with: json[i].U_username,
+                text: "haha"
+              });
+              console.log(json[i].U_username);
+            }
+           
+            //console.log(chatItemList);
+            setRefresh(true);
+            setUsername(org_username)
+          })
+      }
+      
+      
     }
 
     useEffect(()=>{
-      get_allchats();
+      get_allchats();/*.then(()=>{
+        if(typeof org_username != 'undefined'){setUsername(org_username);}
+      });*/
+      
+      
     }, []);
     return (
+      
     <View style={Styles.container}>
       {
+
         refresh &&
       <FlatList
       style={Styles.flatListStyle}
